45590d2b2b4bbf36d8fd30fbb8047c7a10a04cdf
Oskin <wskeal@gmail.com>
2019-12-23 20:17:18 +0300

    updating routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
 Route::middleware('guest')->group(function () {
+    Route::get('tasks', 'API\DownloadTasksController@index');
     Route::post('tasks', 'API\DownloadTasksController@store');
 });
